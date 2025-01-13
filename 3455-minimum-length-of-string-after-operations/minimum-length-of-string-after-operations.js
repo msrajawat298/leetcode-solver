e2555711cfc6c996170dbbdeee3383c255b88841
Mayank Singh Kushwah <msrajawat298@gmail.com>
2025-01-13 12:08:57 +0530

    Time: 126 ms (60.00%) | Memory: 59.3 MB (80.00%) - LeetSync

---
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.js
@@ -0,0 +1,11 @@
+function minimumLength(s) {
+    const cnt = new Map();
+    for (const c of s) {
+        cnt.set(c, (cnt.get(c) || 0) + 1);
+    }
+    let ans = 0;
+    for (const x of cnt.values()) {
+        ans += x % 2 === 1 ? 1 : 2;
+    }
+    return ans;
+}
